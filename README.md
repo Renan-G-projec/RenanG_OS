6c6a545734ce09d77b3a7d427b8f3369a715bf7a
Renan-G-projec <renanfartes@gmail.com>
2026-06-26 20:08:32 -0300

    feat: Restore default background button added. AMDG

--- README.md
+++ README.md
@@ -17,10 +17,10 @@ I used:
 And only these 3. I also used Google fonts for icon searching and font selection.
 
 ## Roadmap
-- [] Custom background feature
-- [] Minimize animations
-- [] Calculator app
-- [] Terminal app
+- [x] Custom background feature
+- [ ] Minimize animations
+- [ ] Calculator app
+- [ ] Terminal app
 
 ## Join Stardance!
 Hack Club is a non-profit organization that helps teens build real stuff. If you're between 13-18 years old, consider jopin us at https://https://hackclub.com/.
--- background.js
+++ background.js
@@ -3,9 +3,18 @@
 const file = document.getElementById("background-image-file-input");
 const uploadButton = document.getElementById("background-image-file-button");
 const previewImg = document.getElementById("preview-background-display");
+const restoreButton = document.getElementById("restore-default-background-button");
 
 function getFile() {
     return file.files[0];
+}
+
+function saveBackground(base64Url) {
+    localStorage.setItem("background-data", base64Url);
+}
+
+function getBackground() {
+    return localStorage.getItem("background-data");
 }
 
 function changeBackground() {
@@ -17,6 +26,7 @@ function changeBackground() {
     const fileReader = new FileReader();
 
     fileReader.onload = () => {
+        saveBackground(fileReader.result);
         document.body.style.backgroundImage = `url(${fileReader.result})`;
     }
 
@@ -34,6 +44,22 @@ function previewBackground() {
     fileReader.readAsDataURL(file);
 }
 
+function syncWithLocalStorage() {
+    const bg = getBackground();
+    if (!bg) return;
+    document.body.style.backgroundImage = `url(${bg})`;
+}
+
+function changeToDefaultBg() {
+    const defaultBg = window.getComputedStyle(document.body).getPropertyValue('--background-image');
+    document.body.style.backgroundImage = defaultBg;
+}
+
+function deleteBg() {
+    localStorage.removeItem("background-data");
+    changeToDefaultBg();
+}
+
 file.addEventListener("change", () => {
     previewBackground();
 })
@@ -41,3 +67,7 @@ file.addEventListener("change", () => {
 uploadButton.addEventListener("click", () => {
     changeBackground();
 })
+
+restoreButton.addEventListener("click", deleteBg)
+
+syncWithLocalStorage();
